53696a850e4380f416a6b63d34eb2ad61cae4a55
EnnioCaprio <40436103+EnnioCaprio@users.noreply.github.com>
2018-10-18 00:04:20 +0200

    site

--- animation.js
+++ animation.js
@@ -1,3 +1,2 @@
-//window.sr = ScrollReveal({ reset: true });
-//sr.reveal('.anim',{scale:1, distance: '120px', origin:'bottom', duration:1500});
-//sr.reveal('.anim2',{scale:1, distance: '60px', origin:'left', duration:1500});
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.anim',{scale:1, distance: '100px', origin:'left', duration:1500});
